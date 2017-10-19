8210c854b4bff62945c5d1f417b48405f2585b23
chenxiao09 <chenxiao09@baidu.com>
2017-10-19 20:25:48 +0800

    项目外壳及构建提交

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app" class="page">
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+  @import "./resource/style/style";
+</style>
---
+++ src/page/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>小智对话框</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    mounted: function () {
+    },
+    methods: {
+    }
+
+}
+</script>
